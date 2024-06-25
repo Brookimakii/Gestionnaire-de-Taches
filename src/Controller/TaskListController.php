8601a70daca32960007a26af7637ba69c576f33e
Ketchus <ketoutou1@gmail.com>
2024-06-25 10:18:16 +0200

    frontv

--- src/Controller/TaskListController.php
+++ src/Controller/TaskListController.php
@@ -3,94 +3,89 @@
 namespace App\Controller;
 
 use App\Entity\TaskList;
-use App\Form\TaskListType;
+use App\Form\ListTaskType;
 use App\Repository\TaskListRepository;
-use App\Repository\UserRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
-
-#[Route('/task/list')]
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 class TaskListController extends AbstractController
 {
-    #[Route('/', name: 'app_task_list_index', methods: ['GET'])]
-    public function index(TaskListRepository $taskListRepository): Response
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        return $this->render('task_list/index.html.twig', [
-            'task_lists' => $taskListRepository->findAll(),
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/tasklists', name: 'tasklist_list')]
+    public function listerListes(): Response
+    {
+        $repository = $this->entityManager->getRepository(TaskList::class);
+        $listes = $repository->findAll();
+
+        return $this->render('tasklist/list.html.twig', [
+            'listes' => $listes,
         ]);
     }
-    #[Route('/private', name: 'app_task_private_list_index', methods: ['GET'])]
-    public function personalList(TaskListRepository $taskListRepository, UserRepository $userRepo): Response
+
+    #[Route('/tasklist/creer', name: 'tasklist_creer')]
+    public function creer(Request $request): Response
+    {
+        $taskList = new TaskList();
+        $form = $this->createForm(ListTaskType::class, $taskList);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->persist($taskList);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('tasklist_detail', ['id' => $taskList->getId()]);
+        }
+
+        return $this->render('tasklist/creer.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/tasklist/{id}', name: 'tasklist_detail')]
+    public function detail(TaskList $taskList): Response
+    {
+        return $this->render('tasklist/detail.html.twig', [
+            'taskList' => $taskList,
+        ]);
+    }
+
+    #[Route('/tasklist/{id}/modifier', name: 'tasklist_modifier')]
+    public function modifier(Request $request, TaskList $taskList): Response
+    {
+        $form = $this->createForm(ListTaskType::class, $taskList);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('tasklist_detail', ['id' => $taskList->getId()]);
+        }
+
+        return $this->render('tasklist/modifier.html.twig', [
+            'taskList' => $taskList,
+            'form' => $form->createView(),
+        ]);
+    }
+    #[Route('/tasklists/private', name: 'app_task_private_list_index', methods: ['GET'])]
+    public function personalList(TaskListRepository $taskListRepository): Response
     {
       return $this->render('task_list/index.html.twig', [
         'task_lists' => $taskListRepository->findPersonalListOfUser($this->getUser()),
       ]);
     }
-    #[Route('/shared', name: 'app_task_shared_list_index', methods: ['GET'])]
-    public function sharedList(TaskListRepository $taskListRepository, UserRepository $userRepo): Response
+    #[Route('/tasklists/shared', name: 'app_task_shared_list_index', methods: ['GET'])]
+    public function sharedList(TaskListRepository $taskListRepository): Response
     {
       return $this->render('task_list/index.html.twig', [
         'task_lists' => $taskListRepository->findSharedListOfUser($this->getUser()),
       ]);
-    }
-
-    #[Route('/new', name: 'app_task_list_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $taskList = new TaskList();
-        $form = $this->createForm(TaskListType::class, $taskList);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($taskList);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_task_list_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('task_list/new.html.twig', [
-            'task_list' => $taskList,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_task_list_show', methods: ['GET'])]
-    public function show(TaskList $taskList): Response
-    {
-        return $this->render('task_list/show.html.twig', [
-            'task_list' => $taskList,
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'app_task_list_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, TaskList $taskList, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(TaskListType::class, $taskList);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_task_list_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('task_list/edit.html.twig', [
-            'task_list' => $taskList,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_task_list_delete', methods: ['POST'])]
-    public function delete(Request $request, TaskList $taskList, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$taskList->getId(), $request->getPayload()->getString('_token'))) {
-            $entityManager->remove($taskList);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('app_task_list_index', [], Response::HTTP_SEE_OTHER);
     }
 }
--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -4,28 +4,47 @@ namespace App\Form;
 
 use App\Entity\Task;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class TaskType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('description')
-            ->add('creationDate', null, [
-                'widget' => 'single_text'
+            ->add('title', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('creation_date', DateType::class, [
+                'widget' => 'single_text',
+                'data' => new \DateTime(),
             ])
-            ->add('priority')
-            ->add('finished')
-            ->add('dueDate', null, [
-                'widget' => 'single_text'
+            
+            ->add('priority', ChoiceType::class, [
+                'choices' => [
+                    'Forte' => 'rouge',
+                    'Moyenne' => 'orange',
+                    'Faible' => 'verte',
+                    'Pas de priorité' => 'gris',
+                ],
+                'placeholder' => 'Choisir une priorité',
+                'required' => false,
+                'expanded' => false,
+                'multiple' => false,
             ])
-        ;
+            ->add('finished', CheckboxType::class, [
+                'required' => false, 
+            ])
+            ->add('due_date', DateType::class, [
+                'widget' => 'single_text',
+            ]);
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
+    public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => Task::class,
