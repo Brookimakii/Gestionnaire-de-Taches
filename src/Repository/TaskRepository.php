da9efdea019da445cda4d850c8153269487209c3
Kenzo Thomias <thomiaskenzo01@gmail.com>
2024-06-26 10:35:59 +0200

    Feat: Adding EndPoint to Task.

--- src/Repository/TaskRepository.php
+++ src/Repository/TaskRepository.php
@@ -1,20 +1,20 @@
 <?php
 
-namespace App\Repository;
+	namespace App\Repository;
 
-use App\Entity\Task;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Persistence\ManagerRegistry;
+	use App\Entity\Task;
+	use App\Entity\TaskList;
+	use App\Entity\User;
+	use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+	use Doctrine\Persistence\ManagerRegistry;
 
-/**
- * @extends ServiceEntityRepository<Task>
- */
-class TaskRepository extends ServiceEntityRepository
-{
-    public function __construct(ManagerRegistry $registry)
-    {
-        parent::__construct($registry, Task::class);
-    }
+	/**
+	 * @extends ServiceEntityRepository<Task>
+	 */
+	class TaskRepository extends ServiceEntityRepository {
+		public function __construct(ManagerRegistry $registry) {
+			parent::__construct($registry, Task::class);
+		}
 
 //    /**
 //     * @return Task[] Returns an array of Task objects
@@ -40,4 +40,26 @@ class TaskRepository extends ServiceEntityRepository
 //            ->getOneOrNullResult()
 //        ;
 //    }
-}
+		/**
+		 * @return Task[] Returns an array of Task objects
+		 */
+		public function getTaskFromList(TaskList $taskList) {
+			return $this->createQueryBuilder('t')
+				->andWhere('t.id = :val')
+				->setParameter('val', $taskList)
+				->orderBy('t.id', 'ASC')
+				->setMaxResults(10)
+				->getQuery()
+				->getResult();
+		}
+
+		public function getTaskAssignTo(User $user, TaskList $taskList) {
+			return $this->createQueryBuilder('t')
+				->andWhere('t.taskList = :taskList')
+				->setParameter('taskList', 1)
+				->orderBy('t.id', 'ASC')
+				->setMaxResults(10)
+				->getQuery()
+				->getResult();
+		}
+	}
